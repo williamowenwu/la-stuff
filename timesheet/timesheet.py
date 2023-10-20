4126bd48865f7957da06500a495ed8667c2c315e
William Wu <willwu1217@gmail.com>
2023-10-20 13:18:35 -0400

    optimized

--- timesheet/timesheet.py
+++ timesheet/timesheet.py
@@ -1,36 +1,42 @@
 import datetime as dt
 import pyperclip
 
-
 def main():
-    offset = dt.timedelta(days=7)
-    today = dt.datetime.today() + offset
+    offset = dt.timedelta(days=7) # to play around with time
+    today = dt.datetime.today() 
 
     day_of_week = today.isocalendar()[2]
     date = today - dt.timedelta(days=day_of_week)
 
-    staff_meetings = [dt.date(2022,11,17), dt.date(2022,12,1)]
+    staff_meetings = [dt.date(2023, 10, 4), dt.date(2023, 10, 18), dt.date(2023, 11, 1), dt.date(2023, 11, 15), dt.date(2023, 12, 6)]
 
     all_days = [((date + dt.timedelta(days=i)).date()) for i in range(7)]
 
-    with open('timesheet.txt','w') as f:
+    # Define the schedule using a dictionary
+    schedule = {
+        'Monday': [
+            "10am - 11am, Prepare for Study Group",
+            "3:50pm - 5:10pm, CS111 Study Group"
+        ],
+        'Wednesday': [
+            "10am - 11am, Staff Meeting" if any(day in staff_meetings for day in all_days) else None,
+            "11am - 12am, Prepare for Study Group",
+            "12:10pm - 1:30pm, CS111 Study Group",
+        ],
+    }
+    
+    print("Hours with students: 3")
+    print(f"Hours in staff meeting: {1 if any(day in staff_meetings for day in all_days) else 0}")
+    print("Hours preparing: 2")
+
+    with open('timesheet.txt', 'w') as f:
         for day in all_days:
             date_month_day = day.strftime("%A %-m/%-d")
-            match day.strftime("%A"):
-                case 'Tuesday':
-                    f.write(f"{date_month_day}, 6 pm - 7pm - Preparation for study group\n")
-                case 'Wednesday':
-                    f.write(f"{date_month_day}, 5:40 pm - 7 pm - CS 112 Study Group\n")
-                case 'Thursday':
-                    if day in staff_meetings:
-                        f.write(f"{date_month_day}, 9 pm - 10 pm - Staff Meeting\n")
-                    f.write(f"{date_month_day}, 5 pm - 6pm - Preparation for Friday Recitations\n")
-                case 'Friday':
-                    f.write(
-                        f"{date_month_day}, 12:25 pm - 1:20 pm - Data Structure Recitation\n"
-                        f"{date_month_day}, 4:05 pm - 5 pm - Data Structure Recitation\n"
-                        f"{date_month_day}, 5:55 pm - 6:50 pm pm - Data Structure Recitation"
-                    )
+            activities = schedule.get(day.strftime("%A"), [])
+            for activity in activities:
+                if activity:
+                    f.write(f"{date_month_day}, {activity}\n")
+
     fo = open('timesheet.txt', 'r').read()
     pyperclip.copy(fo)
 
